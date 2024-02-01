0f075c9154b3b8d7beb6762d4380673de8fd5ad3
Budaházi Bence <budahazi.bence@petrik.hu>
2024-02-01 16:50:46 +0100

    Adatbázis migráció létrehozása

--- database/migrations/2024_02_01_152729_create_people_table.php
+++ database/migrations/2024_02_01_152729_create_people_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('people', function (Blueprint $table) {
             $table->id();
+            $table->string("name", 100);
+            $table->string("email", 200)->unique();
+            $table->string("address", 200);
+            $table->string("phone_number", 20);
+            $table->date("birth_date");
             $table->timestamps();
         });
     }
