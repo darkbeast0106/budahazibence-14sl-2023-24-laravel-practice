1fc9e534739f9d7dbcc9d3e8180582018bf6607f
Budaházi Bence <budahazi.bence@petrik.hu>
2024-02-01 17:06:17 +0100

    Adatbázis feltöltése véletlen teszt adatokkal

--- database/factories/PersonFactory.php
+++ database/factories/PersonFactory.php
@@ -17,7 +17,11 @@ class PersonFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            "name" => fake()->name(),
+            "email" => fake()->email(),
+            "address" => fake()->address(),
+            "phone_number" => fake()->phoneNumber(),
+            "birth_date" => fake()->date()
         ];
     }
 }
